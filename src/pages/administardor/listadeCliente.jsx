f0013d3db6443cbfb35ff986588de9b84bba60f6
Makoto-dev1 <kfzm4k0y@gmail.com>
2022-05-30 21:06:22 -0300

    Alteracaolistagem

--- src/pages/administardor/listadeCliente.jsx
+++ src/pages/administardor/listadeCliente.jsx
@@ -11,41 +11,52 @@ import axios from "axios";
 
 function ListaCadastroClientes() {
     const [users, getUsers] = useState([]);
-    const url = 'https://sb92tpp6dl.execute-api.us-east-1.amazonaws.com/Prod/clientes/adicionargrupo'
+    const [grupo, setGrupo] = useState('');
+    const [email, getEmail] = useState([]);
+    const url = 'https://sb92tpp6dl.execute-api.us-east-1.amazonaws.com/Prod/clientes/grupos'
+    const urll = 'https://sb92tpp6dl.execute-api.us-east-1.amazonaws.com/Prod/clientes'
+    const teste = 'https://sb92tpp6dl.execute-api.us-east-1.amazonaws.com/Prod/teste'
 
     useEffect(() => {
-        listagemUsuarios();
-        // listagemEmail();
-        
+        listagemEmail();
+        //listagemUsuarios();
     }, []);
 
     const listagemUsuarios = () => {
-        axios.get(url)
+        // let config = {
+        //     headers:{
+        //         grupo:Clientes
+        //     }
+        // }
+        axios.get(teste, {
+            headers: {
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Methods': 'GET,OPTIONS',
+                'Content-Type': 'application/json',
+                'grupo': grupo
+            }
+        })
             .then((res) => {
-                const listagemnomes = res.data.data.listagemnomes;
-                getUsers(listagemnomes);
+                const usuarios = res.data.data.usuarios;
+                getUsers(usuarios);
                 console.log(res)
             })
-           
-
+            .then((headers) => {
+                console.log(headers)
+            })
             .catch(error => console.error(`Erro: ${error}`));
-
-        
     }
-    // const listagemEmail = () => {
 
-    //         axios.get(url)
-    //         .then((res1) => {
-    //             const listagememail = res1.data.data.listagememail;
-    //             getUsers(listagememail);
-    //             console.log(res1)
-    //         })
-
-    //         .catch(error => console.error(`Erro: ${error}`));
-
-
-
-    // }
+    const listagemEmail = () => {
+        axios.get(urll)
+            .then((res1) => {
+                const listagememail = res1.data.data.listagememail;
+                getEmail(listagememail);
+            })
+            .catch(error => console.error(`Erro: ${error}`));
+    }
 
     return (
         <div>
@@ -62,49 +73,55 @@ function ListaCadastroClientes() {
                 </div>
             </header>
             <div className='divisaoPrinc2'>
-                
-                    <div className='boneco'>
-                        <img src={boneco} alt="" />
-                    </div>
-                    <div className='blocoCentralPrincipal_Lista_CLientes'>
-                        <div>
-                            <div className='divisao_listagem_cliente'>
-                                <h1 className='lista_do_map'>Lista de Clientes</h1>
-                            </div>
-                            <div >
-                                {
-                                    users.map((res) => {
 
-                                        console.log(res)
-                                        return (
-                                            <div className='blocoCentral_Lista_Clientes'>
-                                                <div key={res.data}>
-                                                    <div className="informacoes">
-                                                        <section className="separacao" >
-                                                            <ul>
-                                                                <li>Nome: {res} </li>
-                                                                {/* <li>Email: {res1}</li>
+                <div className='boneco'>
+                    <img src={boneco} alt="" />
+                </div>
+                <div className='blocoCentralPrincipal_Lista_CLientes'>
+                    <div>
+                        <div className='divisao_listagem_cliente'>
+                            <h1 className='lista_do_map'>Lista de Clientes</h1>
+                        </div>
+                        <div >
+                            <form onSubmit={listagemUsuarios}>
+                                <label htmlFor="text"></label>
+                                <input type="text" name='texto' value={grupo} onChange={(event) => setGrupo(event.target.value)}></input>
+                                <button type='submit' /* value={(users)} onChange={(event) => getUsers(event.target.value)}*/>Teste</button>
+                            </form>
+                            {
+                                users.map((res) => {
+
+                                    console.log(res)
+                                    return (
+                                        <div className='blocoCentral_Lista_Clientes'>
+                                            <div key={res.data}>
+                                                <div className="informacoes">
+                                                    <section className="separacao" >
+                                                        <ul>
+                                                            <li>Nome: {res} </li>
+                                                            {/* <li>Email: {res1}</li>
                                                                 <li>Celuar: {res2}</li>  */}
-                                                            </ul>
-                                                        </section>
-                                                    </div>
+                                                        </ul>
+                                                    </section>
                                                 </div>
-
                                             </div>
 
-                                        )
-                                    })
-                                }
+                                        </div>
 
-                            </div>
+                                    )
+                                })
+                            }
+
                         </div>
                     </div>
-                    <div>
-                        <img className='chat' src={chatboot} alt="" />
-                    </div>
-               
+                </div>
+                <div>
+                    <img className='chat' src={chatboot} alt="" />
+                </div>
+
             </div>
         </div>
     );
 }
-export default ListaCadastroClientes
+
+export default ListaCadastroClientes;
